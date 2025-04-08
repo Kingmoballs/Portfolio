63beaf9b06becfd45a619eefaea2804b8f143d5b
Mobolaji Samuel <141836780+Kingmoballs@users.noreply.github.com>
2025-04-08 11:29:04 +0100

    Thank You page styling

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import "../styles/ThankYou.scss";
 
 export default function ThankYou() {
   return (
-    <div className="container text-center mt-5">
-      <h2 className="text-success">Thank You!</h2>
-      <p>Your message has been sent successfully. I will get back to you soon.</p>
-      <Link to="/" className="btn btn-primary">Go Back to Home</Link>
+    <div className="thank-you-wrapper d-flex justify-content-center align-items-center">
+      <div className="text-center">
+        <h2 className="text-success mb-3">Thank You!</h2>
+        <p className="thank-you-message mb-4">
+          Your message has been sent successfully.<br />I’ll get back to you as soon as possible.
+        </p>
+        <Link to="/" className="btn btn-primary px-4 py-2">
+          Go Back to Home
+        </Link>
+      </div>
     </div>
   );
 }
